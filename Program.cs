0b8b4b08fcfa2931c5d4dc503c12916678fe1670
Nathalia <nathalia_moras@estudante.sesisenai.org.br>
2026-06-11 22:10:06 -0300

    q5

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
-﻿Console.WriteLine("Informe o valor total da compra:");
-        double valorOriginal = double.Parse(Console.ReadLine());
+﻿ Console.WriteLine("Informe a quantidade de tarefas concluídas:");
+        int tarefas = int.Parse(Console.ReadLine());
 
-        double desconto = 0;
-
-        if (valorOriginal >= 200)
+        if (tarefas >= 50)
         {
-            desconto = valorOriginal * 0.10;
+            Console.WriteLine("Meta atingida.");
+            Console.WriteLine("Tarefas concluídas: " + tarefas);
         }
+        else
+        {
+            int faltaram = 50 - tarefas;
 
-        double valorFinal = valorOriginal - desconto;
-
-        Console.WriteLine("Valor original: R$ " + valorOriginal);
-        Console.WriteLine("Desconto aplicado: R$ " + desconto);
-        Console.WriteLine("Valor final: R$ " + valorFinal);
-    
+            Console.WriteLine("Meta não atingida.");
+            Console.WriteLine("Faltaram " + faltaram + " tarefas.");
+        }
