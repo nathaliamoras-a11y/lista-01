62b78ae9303dbf57938851f7766b2a47bff2d12f
Nathalia <nathalia_moras@estudante.sesisenai.org.br>
2026-06-15 20:00:58 -0300

    q14

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
-﻿Console.WriteLine("Informe o número da tabuada:");
-int numero = int.Parse(Console.ReadLine());
+﻿double soma = 0;
 
-for (int i = 1; i <= 10; i++)
-{
-    Console.WriteLine(numero + " x " + i + " = " + (numero * i));
-}
+Console.WriteLine("Informe o primeiro valor:");
+soma += double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo valor:");
+soma += double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o terceiro valor:");
+soma += double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o quarto valor:");
+soma += double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o quinto valor:");
+soma += double.Parse(Console.ReadLine());
+
+double media = soma / 5;
+
+Console.WriteLine("Soma dos valores: " + soma);
+Console.WriteLine("Média dos valores: " + media);
