e6b98e55b049f9b4e22b6c0fdfb9acba1e25e197
Nathalia <nathalia_moras@estudante.sesisenai.org.br>
2026-06-15 19:53:43 -0300

    q13

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-﻿Console.WriteLine("Informe o número inicial da contagem regressiva:");
+﻿Console.WriteLine("Informe o número da tabuada:");
 int numero = int.Parse(Console.ReadLine());
 
-for (int i = numero; i >= 0; i--)
+for (int i = 1; i <= 10; i++)
 {
-    Console.WriteLine(i);
+    Console.WriteLine(numero + " x " + i + " = " + (numero * i));
 }
-
-Console.WriteLine("Fim da contagem.");
