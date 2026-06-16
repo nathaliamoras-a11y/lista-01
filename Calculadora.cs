ca792a4a6792422939e218b71dd57a544474e12a
Nathalia <nathalia_moras@estudante.sesisenai.org.br>
2026-06-15 22:13:57 -0300

    calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,31 @@
+public class Calculadora 
+{
+    public int Somar(int a, int b)
+    {
+        return a + b;
+    }
+     public int Subtrair(int a, int b)
+    {
+        return a - b;
+    }
+    public int Multiplicar(int a, int b)
+    {
+         return a * b;
+    }
+    public int Dividir(int a, int b)
+    {
+         return a / b;
+    }
+    public double Delta(double a, double b, double c)
+    {
+        return Math.Pow(b, 2.0) - (4.0 * a * c);
+    }
+    public double BaskharaX1(double a, double b, double delta)
+    {
+        return ((b * -1.0) + Math.Sqrt(delta)) / (2.0 * a);
+    }
+    public double BaskharaX2(double a, double b, double delta)
+    {
+        return ((b * -1.0) - Math.Sqrt(delta)) / (2.0 * a);
+    }
+}
